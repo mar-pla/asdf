c24fa96e381d398bafd6394a19dfdd1a3d92b51a
mar-pla <marcin.plaza@gmail.com>
2025-06-25 18:34:23 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,8 +9,9 @@ export default function Navbar({ t, lang, setLang }) {
         <Link to="/">{lang === "pl" ? "Strona główna" : "Home"}</Link>
         <Link to="/projekty">{t.projects}</Link>
         <Link to="/realizacje">{t.realizations}</Link>
-        <Link to="/technologie">{t.technologies}</Link>
+        <Link to="/oferta">{t.offer}</Link>
         <Link to="/kontakt">{t.contact}</Link>
+        <Link to="/about">{t.about}</Link>
         <div className="ml-4 space-x-1">
           <button onClick={() => setLang("pl")} className={lang === "pl" ? "font-bold" : ""}>PL</button>
           <button onClick={() => setLang("en")} className={lang === "en" ? "font-bold" : ""}>EN</button>
